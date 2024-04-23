86f78fa41fa7113c240db6efa0f4fe205c72c86d
Alexey Aksionov <alex_331@mail.ru>
2024-04-23 23:51:35 +0300

    Second commit

--- src/main/java/org/example/shop/repository/SimpleRepository.java
+++ src/main/java/org/example/shop/repository/SimpleRepository.java
@@ -1,14 +1,17 @@
 package org.example.shop.repository;
 
+import org.example.shop.model.Brand;
+
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public interface SimpleRepository<T,K> {
-    Optional<T> findById(K id) throws SQLException;
+    Optional<T> findById(K id);
 
     boolean deleteById(K id);
 
-    T findAll();
+    List<T> findAll() ;
 
-    T save(T t);
+    boolean save(T t);
 }
--- src/main/java/org/example/shop/repository/impl/BrandRepositoryImpl.java
+++ src/main/java/org/example/shop/repository/impl/BrandRepositoryImpl.java
@@ -1,13 +1,9 @@
 package org.example.shop.repository.impl;
 
-import org.example.shop.db.ConnectionManagerImpl;
 import org.example.shop.model.Brand;
 import org.example.shop.repository.BrandRepository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,50 +15,66 @@ public class BrandRepositoryImpl implements BrandRepository {
 
     private static final String FIND_BY_ID = "SELECT * FROM Brand WHERE idbrand = ?";
     private static final String FIND_ALL = "SELECT * FROM Brand";
-
+    private static final String DELETE_BY_ID = "DELETE * FROM Brand WHERE idbrand = ?";
+    private static final String SAVE = "INSERT INTO Brand(idbrand,brandName) VALUES = (?,?)";
     public BrandRepositoryImpl(Connection connection) {
         this.connection = connection;
     }
 
     @Override
-    public Optional<Brand> findById(Long id) throws SQLException {
+    public Optional<Brand> findById(Long id)  {
         try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID)) {
             preparedStatement.setLong(1, id);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            ResultSet resultSet = preparedStatement.executeQuery();
                 if (resultSet.next()) {
                     Brand brand = new Brand();
                     brand.setBrandName(resultSet.getString("brand_name"));
                     brand.setIdBrand(resultSet.getLong("id_brand"));
                     return Optional.of(brand);
                 }
-            }
-            return Optional.empty();
+                     return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
         @Override
-        public boolean deleteById (Integer id){
-            return false;
+        public boolean deleteById (Long id){
+            try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BY_ID)) {
+                preparedStatement.setLong(1, id);
+                return preparedStatement.executeUpdate()>0;
+                }
+            catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         @Override
         public List<Brand> findAll () {
             List<Brand>brands=new ArrayList<>();
             try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL)) {
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+               ResultSet resultSet = preparedStatement.executeQuery();
                     while (resultSet.next()) {
                         Brand brand = new Brand();
                         brand.setBrandName(resultSet.getString("brand_name"));
                         brand.setIdBrand(resultSet.getLong("id_brand"));
-
+                        brands.add(brand);
                     }
-                }
                 return brands;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
         }
 
         @Override
-        public Brand save (Brand brand){
-            return null;
+        public boolean save (Brand brand){
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SAVE)) {
+                preparedStatement.setLong(1, Statement.RETURN_GENERATED_KEYS);
+                preparedStatement.setString(2, brand.getBrandName());
+                return preparedStatement.executeUpdate()>0;
+            }
+            catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
